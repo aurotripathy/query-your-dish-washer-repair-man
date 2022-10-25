245079b2f204c8582d66bb18ccc70505fdcb847e
Auro Tripathy <ipserv@yahoo.com>
2022-10-24 22:15:46 -0700

    init

--- parser.py
+++ parser.py
@@ -4,38 +4,37 @@ import requests
 from bs4 import BeautifulSoup, NavigableString, Tag
 
 
-def gen_text_for_embeddings(url, out_file):
+def gen_text_for_embeddings(url, f):
     
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'html.parser')
 
     allowlist = ['p',]
 
-    
-    with open(out_file, 'w') as f:
+    f.write(f'*************Table-of-Contents*************\n')
+    for div in soup.findAll('div', {'class': 'toc-category'}):
+        f.write(f'{str(div.find_all(text=True))}\n')
 
-        f.write(f'*************Table-of-Contents*************\n')
-        for div in soup.findAll('div', {'class': 'toc-category'}):
-            f.write(f'{str(div.find_all(text=True))}\n')
-
-        soup = BeautifulSoup(html_text, 'html.parser')
-        for i, para in enumerate(soup.findAll('p')):
-            # print(f'***{i}***')
-            text = ' '
-            for child in para:
-                if isinstance(child, NavigableString):
-                    text += str(child).strip()
-                elif isinstance(child, Tag):
-                    if child.name != 'br' or child.name != 'em':
-                        text = text + ' ' + child.text + ' '
-                    else:
-                        text += '\n'
-            # result = text.strip().split('\n')
-            # print(text)
-            f.write(f'{text}\n')
+    soup = BeautifulSoup(html_text, 'html.parser')
+    for i, para in enumerate(soup.findAll('p')):
+        # print(f'***{i}***')
+        text = ' '
+        for child in para:
+            if isinstance(child, NavigableString):
+                text += str(child).strip()
+            elif isinstance(child, Tag):
+                if child.name != 'br' or child.name != 'em':
+                    text = text + ' ' + child.text + ' '
+                else:
+                    text += '\n'
+        # result = text.strip().split('\n')
+        # print(text)
+        f.write(f'{text}\n')
 
 # main
 out_file = 'out-file.txt'   
+f_handle = open(out_file, 'w')
 for i in range(1, 6):
     url = f'https://www.appliancerepair.net/dishwasher-repair-{i}.html'
-    gen_text_for_embeddings(url, out_file)   
+    gen_text_for_embeddings(url, f_handle)   
+f_handle.close()
